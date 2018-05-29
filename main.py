e0abf0cbbb2f42bda2ae5955cff4f9aabc790c6e
Lindeza Grey <andyvziegler@web.de>
2018-05-29 20:29:39 +0200

    First test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from threading import Thread
 
 
 class Controller(object):
@@ -15,28 +16,38 @@ class Controller(object):
         self._axis_power = axis_pow
         self.output_left_right = 0
         self.output_power = 0
-        self.power_modifier = 0.8  # limit the total output 1 = no limit
-        self.start()
+        self.power_modifier = 0.9  # limit the total output 1 = no limit
+        self._polling_thread = Thread(target=self.start,)
+        self._polling_thread.start()
+
+        self._output_thread = Thread(target=self.send_output, )
+        self._output_thread.start()
 
     def start(self):
         while self._running:
             for event in pygame.event.get():  # User did something
-                # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-                if event.type == pygame.JOYBUTTONDOWN:
-                    print("Joystick button pressed.")
-                if event.type == pygame.JOYBUTTONUP:
-                    print("Joystick button released.")
-
-                self.output_left_right = self._joystick.get_axis(self._axis_left_right) * 1000 * self.power_modifier
-                self.output_power = self._joystick.get_axis(self._axis_power) * 1000 * self.power_modifier
-                print(self.output_left_right)
+                # print(event)
+                self.output_left_right = self.calculate_power(self._joystick.get_axis(self._axis_left_right))
+                self.output_power = self.calculate_power(self._joystick.get_axis(self._axis_power))
 
                 # button = self._joystick.get_button(i)
 
-                # Limit polling rate
-                time.sleep(0.2)
-
+            # Limit polling rate
+            # time.sleep(0.2)
         pygame.quit()
+
+    def calculate_power(self, value):
+        x = round(value, 2) * 1000 * self.power_modifier
+        return x
+
+    def send_output(self):
+        # sends output via I2C
+        while True:
+            print(self.output_power, self.output_left_right)
+            time.sleep(1)
 
 
 xbox = Controller()
+while 1:
+    time.sleep(0.2)
+    # print(xbox.output_left_right)
